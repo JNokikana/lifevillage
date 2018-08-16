3f14dbe82ad8edc11a657d903c435b1dcf465587
Joonas Nousiainen <herrapentti@gmail.com>
2018-08-16 19:58:56 +0300

    Structure is nice and sockets work.

--- webapp/components/App.jsx
+++ webapp/components/App.jsx
@@ -1,20 +1,35 @@
 import React from 'react';
 import Const from '../util/const.js';
-import * as SocketHandler from '../controller/socker-handler.js';
-
 let ws;
 
 class App extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            message: "Kikkeli"
+        }
+
+        this.openConnection = this.openConnection.bind(this);
+        this.receiveMessage = this.receiveMessage.bind(this);
+
         ws = new WebSocket(Const.API_PATH);
-        ws.addEventListener('open', SocketHandler.openConnection);
-        ws.addEventListener('message', SocketHandler.receiveMessage);
+        ws.addEventListener("open", this.openConnection);
+        ws.addEventListener("message", this.receiveMessage);
+    }
+    openConnection(event) {
+        console.log("Marco");
+        ws.send(JSON.stringify({ message: 'Marco' }));
+    }
+    receiveMessage(event){
+        console.log("Polo", event.data);
+        this.setState({message: event.data})
     }
     render() {
         return (
             <div>
                 Moro
+                {this.state.message}
             </div>
         );
     }
--- webapp/controller/socker-handler.js
+++
@@ -1,7 +0,0 @@
-export function openConnection(event) {
-    console.log("Marco");
-    ws.send(JSON.stringify({ message: 'Marco' }));
-}
-export function receiveMessage(event) {
-    console.log("Polo", event.data);
-}
