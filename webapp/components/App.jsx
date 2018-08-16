da227a5bb1ee1b01cbdf65eef0e9389f75c4cfa8
Joonas Nousiainen <herrapentti@gmail.com>
2018-08-16 18:40:48 +0300

    Websocket stuff.

--- webapp/components/App.jsx
+++ webapp/components/App.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import Const from '../util/const.js';
+
+let ws;
 
 class App extends React.Component {
+    constructor(props){
+        super(props);
+        ws = new WebSocket(Const.API_PATH);
+    }
+    componentDidMount(){
+        
+    }
     render() {
         return (
             <div>
--- webapp/public/index.jsx
+++ webapp/public/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../components/App.jsx';
-import Demo from '../public/assets/demo/demo.jsx';
 
 ReactDOM.render(
     <App />,
